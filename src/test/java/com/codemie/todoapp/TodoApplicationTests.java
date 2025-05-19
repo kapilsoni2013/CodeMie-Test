c34c77d3f4940866bfaf4865b1a38491a1915447
Kapil Sony A White Hat Hacker <kapilsoni2013@gmail.com>
2025-05-19 14:50:17 +0530

    Create TodoApplicationTests.java

---
+++ src/test/java/com/codemie/todoapp/TodoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.codemie.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
