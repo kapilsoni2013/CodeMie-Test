0b9ad0210ecb2f74a51901774d53ac7c0d72d3c0
Kapil Verma <kapil_verma1@epam.com>
2025-06-04 04:57:22 +0530

    reverted

--- src/main/java/com/example/springbootjira/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbootjira.repository;
-
-import com.example.springbootjira.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
--- src/test/java/com/example/springbootjira/SpringBootJiraApplicationTests.java
+++ src/test/java/com/example/springbootjira/SpringBootJiraApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.springbootjira;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class SpringBootJiraApplicationTests {
 
