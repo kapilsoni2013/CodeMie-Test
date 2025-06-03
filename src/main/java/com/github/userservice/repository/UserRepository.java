998ff0bf78db29f74031c5655b100fa94da82531
Kapil Sony A White Hat Hacker <kapilsoni2013@gmail.com>
2025-06-04 05:19:16 +0530

    Revert "Implement Registration and Task Functionality"

--- src/main/java/com/github/userservice/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.userservice.repository;
-
-import com.github.userservice.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
