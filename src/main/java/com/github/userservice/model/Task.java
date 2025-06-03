4fb8e1696d911ed005e7f2eea64ec59b0286bccd
Kapil Sony A White Hat Hacker <kapilsoni2013@gmail.com>
2025-06-04 05:16:01 +0530

    Add Task entity class

---
+++ src/main/java/com/github/userservice/model/Task.java
@@ -0,0 +1,21 @@
+package com.github.userservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+
+    // Getters and Setters
+}
