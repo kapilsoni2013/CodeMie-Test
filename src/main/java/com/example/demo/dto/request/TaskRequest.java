01d85694b08031342a78812b1e36d1084b72ad59
Kapil Sony A White Hat Hacker <kapilsoni2013@gmail.com>
2025-06-04 05:57:03 +0530

    Added TaskRequest.java

---
+++ src/main/java/com/example/demo/dto/request/TaskRequest.java
@@ -0,0 +1,24 @@
+package com.example.demo.dto.request;
+
+public class TaskRequest {
+    private String title;
+    private String description;
+
+    // Getters and Setters
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
