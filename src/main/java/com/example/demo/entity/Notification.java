36bc41ec5d5d6da15b8047bf61eee0ae9efd603a
Kapil Sony A White Hat Hacker <kapilsoni2013@gmail.com>
2025-06-04 06:11:32 +0530

    Feature-123: Add Notification entity

---
+++ src/main/java/com/example/demo/entity/Notification.java
@@ -0,0 +1,16 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "notifications")
+@Data
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String message;
+    private boolean read;
+}
