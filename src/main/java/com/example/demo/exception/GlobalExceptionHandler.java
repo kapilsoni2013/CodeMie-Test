30ae2ed24d99344cb36dd85b252c86c9fd4289af
Kapil Sony A White Hat Hacker <kapilsoni2013@gmail.com>
2025-06-04 05:57:26 +0530

    Added GlobalExceptionHandler.java

---
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.demo.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(Exception ex, WebRequest request) {
+        return ResponseEntity.status(500).body(ex.getMessage());
+    }
+}
