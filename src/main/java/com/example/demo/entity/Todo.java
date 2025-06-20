6d971dbb9fcec2f1fde041c56c5ffec044825caf
Kapil Sony A White Hat Hacker <kapilsoni2013@gmail.com>
2025-06-21 04:20:47 +0530

    Extracted database table and column names as constants in Todo entity class

--- src/main/java/com/example/demo/entity/Todo.java
+++ src/main/java/com/example/demo/entity/Todo.java
@@ -4,19 +4,27 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
-@Table(name = "todos")
+@Table(name = Todo.TABLE_NAME)
 @Data
 public class Todo {
+    
+    public static final String TABLE_NAME = "todos";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_TITLE = "title";
+    public static final String COLUMN_DESCRIPTION = "description";
+    public static final String COLUMN_COMPLETED = "completed";
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = COLUMN_ID)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(name = COLUMN_TITLE, nullable = false)
     private String title;
 
-    @Column(nullable = false)
+    @Column(name = COLUMN_DESCRIPTION, nullable = false)
     private String description;
 
-    @Column(nullable = false)
+    @Column(name = COLUMN_COMPLETED, nullable = false)
     private boolean completed;
 }
