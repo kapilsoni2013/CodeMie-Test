1f85d94fb0d2b26f2231890933867fd8ef92259b
Kapil Sony A White Hat Hacker <kapilsoni2013@gmail.com>
2025-06-21 04:19:59 +0530

    Refactored string literals to use constants in Base64Request.java

--- src/main/java/com/example/demo/dto/Base64Request.java
+++ src/main/java/com/example/demo/dto/Base64Request.java
@@ -3,7 +3,9 @@ package com.example.demo.dto;
 import jakarta.validation.constraints.NotNull;
 
 public class Base64Request {
-    @NotNull(message = "Input string cannot be null")
+    private static final String NULL_INPUT_ERROR_MESSAGE = "Input string cannot be null";
+
+    @NotNull(message = NULL_INPUT_ERROR_MESSAGE)
     private String input;
 
     public String getInput() {
